7ec3fcbca2b3dc0e72a0f28d723d18fb583c1a3a
Claudia Lam <claudiaslam@gmail.com>
2023-03-27 21:58:07 -0700

    append gifs to giphy container

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
 "use strict";
 
-"hello";
+const GIPHY_BASE_URL = "http://api.giphy.com/v1";
+const GIPHY_API_KEY = "u0m6RVDC8jN9C65VwgvPFerw8SW6Pqyd";
+
 async function getGiphy(evt) {
   evt.preventDefault();
 
-  let search = $("#search").val();
+  let searchTerm = $("#search-form").val();
 
-  let response = await axios.get("http://api.giphy.com/v1/gif/search?", {
-    params: { search },
+  const response = await axios.get(`${GIPHY_BASE_URL}/gifs/search`, {
+    params: { q: searchTerm, api_key: GIPHY_API_KEY },
   });
 
-  console.log("response", response);
+  console.log("response", response.data.data);
 
-  $(".giphy-container").html(response.data);
+  const randomGif = pickRandomGif(response.data.data);
+  console.log(randomGif);
+
+  const gifUrl = response.data.data[randomGif].images.original.url;
+  console.log(gifUrl);
+
+  const newImg = createImg(gifUrl);
+
+  $(".giphy-container").append(newImg);
 }
 
-$(".form").submit(getGiphy);
+function pickRandomGif(responseData) {
+  return Math.floor(Math.random() * responseData.length);
+}
+
+function createImg(gifUrl) {
+  const $img = $('<img id="giphy-img">');
+  $img.attr("src", gifUrl);
+  return $img;
+}
+
+$("#search").submit(getGiphy);
